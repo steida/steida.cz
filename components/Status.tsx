81447ba4d2278cd400d46e2df238caf8d74aaca2
Daniel Steigerwald <daniel@steigerwald.cz>
2020-09-19 23:51:06 +0200

    remove some statuses

--- components/Status.tsx
+++ components/Status.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FormattedDate } from 'react-intl';
 import styles from './Status.module.css';
 
+const foo: string[] = [];
+
 export const Status = ({
   id,
   createdTime,
@@ -10,22 +12,29 @@ export const Status = ({
   id: string;
   createdTime: string;
   message: string;
-}) => (
-  <div key={id} className={styles.status}>
-    <a
-      target="_blank"
-      href={`https://www.facebook.com/daniel.steigerwald/posts/${
-        id.split('_')[1]
-      }`}
-      className={styles.timeLink}
-    >
-      <FormattedDate
-        value={createdTime}
-        year="numeric"
-        month="long"
-        day="numeric"
-      />
-    </a>
-    <div className={styles.message}>{message}</div>
-  </div>
-);
+}) => {
+  const handleClick = () => {
+    foo.push(id);
+    console.log(foo);
+  };
+
+  return (
+    <div key={id} className={styles.status} onClick={handleClick}>
+      <a
+        target="_blank"
+        href={`https://www.facebook.com/daniel.steigerwald/posts/${
+          id.split('_')[1]
+        }`}
+        className={styles.timeLink}
+      >
+        <FormattedDate
+          value={createdTime}
+          year="numeric"
+          month="long"
+          day="numeric"
+        />
+      </a>
+      <div className={styles.message}>{message}</div>
+    </div>
+  );
+};
